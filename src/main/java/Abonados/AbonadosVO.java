d7ffaddc53f6b69bf0e0ee117a749c0a5aebcb8f
javier <javiermedmata@gmail.com>
2019-05-16 14:39:32 +0200

    Clase abonados con sus atributos

---
+++ src/main/java/Abonados/AbonadosVO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abonados;
+
+/**
+ *
+ * @author javi
+ */
+public class AbonadosVO {
+     private int codAbonados;
+    private String DNI;
+    private String nombre;
+    private String apellidos;
+    private int pinAbonados;
+
+}
