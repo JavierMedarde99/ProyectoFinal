8e5afa43b3f522f73c7a61a49e05ecda97d3736d
javier <javiermedmata@gmail.com>
2019-05-16 14:42:19 +0200

    Constructor

--- src/main/java/Abonados/AbonadosVO.java
+++ src/main/java/Abonados/AbonadosVO.java
@@ -10,10 +10,21 @@ package Abonados;
  * @author javi
  */
 public class AbonadosVO {
-     private int codAbonados;
+     private int codAbonados=0;
     private String DNI;
     private String nombre;
     private String apellidos;
     private int pinAbonados;
+    
+public AbonadosVO() {
+    }
 
+    public AbonadosVO( String nombre, String apellidos,String DNI, int pinAbonados) {
+        this.codAbonados=codAbonados;
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.pinAbonados = pinAbonados;
+        codAbonados++;
+    }
 }
