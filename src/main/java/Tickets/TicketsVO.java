f0e21f921630dcee66a7341604f02f5c42e1c60c
javi <javiermedmata@gmail.com>
2019-05-17 17:52:17 +0200

    clase Tickets completa

---
+++ src/main/java/Tickets/TicketsVO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tickets;
+
+/**
+ *
+ * @author javie
+ */
+public class TicketsVO {
+    private int codPlazas;
+    private String matricula;
+    private int pin;
+    private int precio;
+    
+    
+}
