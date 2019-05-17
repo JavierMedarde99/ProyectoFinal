3eb69169fcbd0e16ed0779a12bd45e2f275b63cc
javi <javiermedmata@gmail.com>
2019-05-17 20:52:57 +0200

    nuevos atributos Abonados

--- src/main/java/Abonados/AbonadosVO.java
+++ src/main/java/Abonados/AbonadosVO.java
@@ -10,30 +10,35 @@ package Abonados;
  * @author javi
  */
 public class AbonadosVO {
-     private int codAbonados=0;
+     private int codAbonados=0;//clave primaria
     private String DNI;
     private String nombre;
     private String apellidos;
     private int pinAbonados;
-    
+    private String tarjetaCredito;
+    private String email;
+    private int tipoAbonados;
+    private String matricula;//clave foreanea
 public AbonadosVO() {
     }
 
-    public AbonadosVO( String nombre, String apellidos,String DNI, int pinAbonados) {
-        this.codAbonados=codAbonados;
+    public AbonadosVO(String DNI, String nombre, String apellidos, int pinAbonados, String tarjetaCredito, String email, int tipoAbonados, String matricula) {
         this.DNI = DNI;
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.pinAbonados = pinAbonados;
-        codAbonados++;
+        this.tarjetaCredito = tarjetaCredito;
+        this.email = email;
+        this.tipoAbonados = tipoAbonados;
+        this.matricula = matricula;
     }
-    
-     public  int getCodAbonado() {
+
+    public int getCodAbonados() {
         return codAbonados;
     }
 
-    public  void setCodAbonado(int codAbonado) {
-        this.codAbonados = codAbonado;
+    public void setCodAbonados(int codAbonados) {
+        this.codAbonados = codAbonados;
     }
 
     public String getDNI() {
@@ -68,8 +73,42 @@ public AbonadosVO() {
         this.pinAbonados = pinAbonados;
     }
 
+    public String getTarjetaCredito() {
+        return tarjetaCredito;
+    }
+
+    public void setTarjetaCredito(String tarjetaCredito) {
+        this.tarjetaCredito = tarjetaCredito;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTipoAbonados() {
+        return tipoAbonados;
+    }
+
+    public void setTipoAbonados(int tipoAbonados) {
+        this.tipoAbonados = tipoAbonados;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
     @Override
     public String toString() {
-        return "AbonadosVO{" + "DNI=" + DNI + ", nombre=" + nombre + ", apellidos=" + apellidos + ", pinAbonados=" + pinAbonados + '}';
+        return "AbonadosVO{" + "codAbonados=" + codAbonados + ", DNI=" + DNI + ", nombre=" + nombre + ", apellidos=" + apellidos + ", pinAbonados=" + pinAbonados + ", tarjetaCredito=" + tarjetaCredito + ", email=" + email + ", tipoAbonados=" + tipoAbonados + ", matricula=" + matricula + '}';
     }
+
+    
 }
