90dc435070d4fa247b509ce5ca07084e9cb26863
javi <javiermedmata@gmail.com>
2019-05-17 17:53:10 +0200

    clase con todo los metodos TicketsVO

--- src/main/java/Tickets/TicketsVO.java
+++ src/main/java/Tickets/TicketsVO.java
@@ -14,6 +14,52 @@ public class TicketsVO {
     private String matricula;
     private int pin;
     private int precio;
+
+    public TicketsVO(int codPlazas, String matricula, int pin, int precio) {
+        this.codPlazas = codPlazas;
+        this.matricula = matricula;
+        this.pin = pin;
+        this.precio = precio;
+    }
+
+    public int getCodPlazas() {
+        return codPlazas;
+    }
+
+    public void setCodPlazas(int codPlazas) {
+        this.codPlazas = codPlazas;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "TicketsVO{" + "codPlazas=" + codPlazas + ", matricula=" + matricula + ", pin=" + pin + ", precio=" + precio + '}';
+    }
+    
+    
     
     
 }
