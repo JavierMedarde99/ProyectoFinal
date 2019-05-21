adb061d96d97e0c15a9a98a2443770d301ad9909
jriosaguilar <josemanueljbk99@gmail.com>
2019-05-21 08:57:19 +0200

    Añade métodos a la interfaz

--- src/main/java/vehiculos/IVehiculo.java
+++ src/main/java/vehiculos/IVehiculo.java
@@ -5,10 +5,28 @@
  */
 package vehiculos;
 
+import java.sql.SQLException;
+import java.util.List;
+import plazas.PlazasVO;
+
 /**
  *
  * @author jriosaguilar
  */
-public class IVehiculo {
+public interface IVehiculo {
+    
+    List<VehiculoVO> getAll() throws SQLException;
+    
+    int insertVehiculo(VehiculoVO vehiculo) throws SQLException;
+    
+    VehiculoVO findByPk(String matricula) throws SQLException;
+    
+    int insertVehiculo(List<VehiculoVO> lista) throws SQLException;
+    
+    int deleteVehiculo(VehiculoVO vehiculo) throws SQLException;
+    
+    int deleteVehiculo() throws SQLException;
+    
+    int updateVehiculo(String vehiculo, PlazasVO nuevosDatos) throws SQLException;
     
 }
