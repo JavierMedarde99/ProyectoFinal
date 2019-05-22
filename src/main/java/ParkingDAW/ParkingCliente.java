5582dd5bb6931749c302d50bcd0409e724eb7478
javier <javiermedmata@gmail.com>
2019-05-22 10:43:02 +0200

    control sonre los switch

--- src/main/java/ParkingDAW/ParkingCliente.java
+++ src/main/java/ParkingDAW/ParkingCliente.java
@@ -5,12 +5,63 @@
  */
 package ParkingDAW;
 
+import java.util.Scanner;
+
 /**
  *
  * @author javi
  */
-public class Parking {
+public class ParkingCliente {
     public static void main(String[] args) {
-        
+        int Elecion1,Elecion2,Elecion3;
+        do{
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("1.Si es abonado de nuestro Parking");
+        System.out.println("2.No soy abonado y deseo insertar mi vehiculo");
+        System.out.println("3.No soy abonado y deseo serlo");
+         Elecion1 = teclado.nextInt();
+        switch (Elecion1) {
+            case 1:
+                do{
+                System.out.println("1.Deseo insertar mi vehiculo");
+                System.out.println("2.Deseo retirar mi vehiculo");
+                Elecion2 = teclado.nextInt();
+                switch (Elecion2) {
+                    case 1:
+                        
+                        break;
+                     case 2:
+                        
+                        break;
+                    default:
+                        Elecion2=0;
+                }
+                }while(Elecion2==0);
+                break;
+                case 2:
+                 do{
+                System.out.println("1.Deseo insertar mi vehiculo");
+                System.out.println("2.Deseo retirar mi vehiculo");
+                Elecion3 = teclado.nextInt();
+                switch (Elecion3) {
+                    case 1:
+                        
+                        break;
+                     case 2:
+                        
+                        break;
+                    default:
+                        Elecion3=0;
+                }
+                }while(Elecion3==0);
+                break;
+                case 3:
+                    
+                    
+                break;
+            default:
+               Elecion1=0;
+        }
+        }while(Elecion1==0);
     }
 }
