fb3b42061e087d0b1caa412d6cef0e0d306980a1
jriosaguilar <josemanueljbk99@gmail.com>
2019-05-22 12:45:18 +0200

    Añade método para insertar datos de un nuevo abonado

--- src/main/java/Abonados/MetodosAbonados.java
+++ src/main/java/Abonados/MetodosAbonados.java
@@ -5,7 +5,10 @@
  */
 package Abonados;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -13,6 +16,7 @@ import java.util.ArrayList;
  */
 public class MetodosAbonados {
     
+    //Método para comprobar si el abonado está dentro de la lista de abonados
     public static boolean comprobarAbonado(ArrayList<AbonadosVO> abonados, String dni){
         for (AbonadosVO tmp : abonados) {
             if(tmp.getDNI().equalsIgnoreCase(dni)){
@@ -20,6 +24,63 @@ public class MetodosAbonados {
             }       
         }
         return false;
+    }
+    
+    public static AbonadosVO insertarDatos(){
+        Scanner teclado = new Scanner(System.in);
+        Random rnd = new Random();
+        
+        int Pin = rnd.nextInt(111111-999999);
+        
+        System.out.println("DNI:");
+        String DNI=teclado.next();
+        
+        System.out.println("Nombre:");
+        String Nombre=teclado.next();
+        
+        System.out.println("Apellidos:");
+        String Apellidos=teclado.next();
+        
+        System.out.println("Tarjeta de credito:");
+        String Tarjeta=teclado.next();
+        
+        System.out.println("email:");
+        String Email=teclado.next();
+        
+        System.out.println("Que tipo de abonado quiere");
+        System.out.println("1.anual");
+        System.out.println("2.trimestral");
+        System.out.println("3.mensual");
+        System.out.println("4.semanal");
+        int TipoAbonado;
+        
+        do{
+            TipoAbonado=teclado.nextInt();
+            switch (TipoAbonado) {
+                case 1:
+                    TipoAbonado=1;
+                    break;
+
+                case 2:
+                    TipoAbonado=2;
+                    break;
+
+                case 3:
+                    TipoAbonado=3;
+                    break;
+
+                case 4:
+                    TipoAbonado=4;
+                    break;
+                default:
+                    TipoAbonado=0;
+            }
+        }while(TipoAbonado==0);
+        
+        System.out.println("matricula de su coche:");
+        String Matricula=teclado.next();
+        
+        return new AbonadosVO(DNI,Nombre,Apellidos,Pin,Tarjeta,Email,TipoAbonado,Matricula,LocalDate.now());
     }
     
 }
