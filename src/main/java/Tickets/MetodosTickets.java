6d24b7857e9173e579d5ff6d70bebb4a4bd7d748
jriosaguilar <josemanueljbk99@gmail.com>
2019-05-23 13:56:17 +0200

    Modifica método retirarTicket

--- src/main/java/Tickets/MetodosTickets.java
+++ src/main/java/Tickets/MetodosTickets.java
@@ -5,7 +5,9 @@
  */
 package Tickets;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import vehiculos.VehiculoVO;
 
@@ -15,8 +17,9 @@ import vehiculos.VehiculoVO;
  */
 public class MetodosTickets {
     
-    public static long retirarTicket(LocalDateTime tiempoInicio){
-        return ChronoUnit.MINUTES.between(tiempoInicio, LocalDateTime.now());
+    public static long retirarTicket(LocalTime tiempoInicio, LocalDate fechaInicio){
+        LocalDateTime horaEntrada=LocalDateTime.of(fechaInicio, tiempoInicio);
+        return ChronoUnit.MINUTES.between(horaEntrada, LocalDateTime.now());
     }
     
     public static Double calcularPrecioMinuto(VehiculoVO vehiculo){
@@ -24,13 +27,13 @@ public class MetodosTickets {
             case 1:
                 return 0.12;
             case 2:
-                return 0.08;
+                return 0.008;
             case 3:
                 return 0.45;
             default:
                 System.out.println("El tipo de vehiculo es erroneo");
         }
-        return 0.0;
+        return 0.0;  
     }
     
 }
