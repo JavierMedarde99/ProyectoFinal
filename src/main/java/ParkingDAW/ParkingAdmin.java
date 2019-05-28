2a445af0656c66991e8ca77a320a7b1baea51333
javier <javiermedmata@gmail.com>
2019-05-28 12:19:37 +0200

    clase para la zona admin

---
+++ src/main/java/ParkingDAW/ParkingAdmin.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ParkingDAW;
+
+import plazas.PlazasVO;
+
+/**
+ *
+ * @author javi
+ */
+public class ParkingAdmin {
+    public static void main(String[] args) {
+        
+    }
+}
