2b0b8ad3b90425759957e5cddaf087de1a2c3a25
javier <javiermedmata@gmail.com>
2019-05-29 12:46:46 +0200

    completar metodo

--- src/main/java/Admin/Admin.java
+++ src/main/java/Admin/Admin.java
@@ -6,9 +6,12 @@
 package Admin;
 
 import Abonados.AbonadosVO;
+import Tickets.TicketsVO;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
 /**
  *
@@ -19,9 +22,11 @@ public class Admin {
          
            
     }
-    public static int PrecioEntreDosFechas(LocalDate fecha1 ,LocalDate fecha2,AbonadosVO abonado){
-        if(abonado.getFechaInicioAbono().equals(ChronoUnit.YEARS.between(fecha1, fecha2)) && abonado.getFechaFinAbono().equals(ChronoUnit.YEARS.between(fecha1, fecha2))){
-            
+    public static ArrayList<Integer> PrecioEntreDosFechas(LocalDate fecha1 ,LocalDate fecha2,LocalTime tiempo1,LocalTime tiempo2, TicketsVO ticket){
+        ArrayList<Integer> precio = new ArrayList<>();
+        if(ticket.getFechaInicio().equals(ChronoUnit.YEARS.between(fecha1, fecha2)) && ticket.getFechaFin().equals(ChronoUnit.YEARS.between(fecha1, fecha2))
+                && ticket.getTiempoInicio().equals(ChronoUnit.HOURS.between(tiempo1, fecha2)) && ticket.getTiempoFin().equals(ChronoUnit.HOURS.between(tiempo1, fecha2))){
+           
         }
     }
 }
