41f0f6a128ae437529cb9a9d0342f43cc869ba84
jriosaguilar <josemanueljbk99@gmail.com>
2019-05-30 13:35:08 +0200

    Completa casos del parking (falta copia seguridad)

--- src/main/java/ParkingDAW/ParkingCliente.java
+++ src/main/java/ParkingDAW/ParkingCliente.java
@@ -15,6 +15,10 @@ import java.util.Scanner;
 import vehiculos.MetodosVehiculos;
 import vehiculos.VehiculoDAO;
 import vehiculos.VehiculoVO;
+import Admin.Admin;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
 
 /**
  *
@@ -23,12 +27,19 @@ import vehiculos.VehiculoVO;
 public class ParkingCliente {
 
     public static void main(String[] args) throws SQLException {
-        int Eleccion1, Eleccion2, Eleccion3, Eleccion4, repetir, tipo, codPlaza, pin;
+        System.out.println("1----------------");
+        int Eleccion1, Eleccion2, Eleccion3, Eleccion4, repetir, tipo, codPlaza, pin, mes;
+        System.out.println("2----------------");
         String dni, matricula;
+        System.out.println("3-----------------");
         AbonadosDAO a1=new AbonadosDAO();
+        System.out.println("4-----------------");
         VehiculoDAO v1=new VehiculoDAO();
+        System.out.println("5-----------------");
         ArrayList<AbonadosVO> listaAbonados = new ArrayList<>();
+        System.out.println("6-----------------");
         Scanner teclado = new Scanner(System.in);
+        System.out.println("7-----------------");
         
         do {
             System.out.println("1. Cliente");
@@ -127,9 +138,25 @@ public class ParkingCliente {
                                 }while(Eleccion3==0);
 
                             case 3:
+                                System.out.println("Introduce un DNI: ");
+                                teclado.nextLine();
+                                dni=teclado.nextLine();
+                                System.out.println("Introduce una matricula: ");
+                                matricula=teclado.nextLine();
+                                System.out.println("Introduce el pin: ");
+                                pin=teclado.nextInt();
+                                MetodosVehiculos.depositarVehiculoAbonado(dni, matricula, pin);
                                 break;
 
                             case 4:
+                                System.out.println("Introduce un DNI: ");
+                                teclado.nextLine();
+                                dni=teclado.nextLine();
+                                System.out.println("Introduce una matricula: ");
+                                matricula=teclado.nextLine();
+                                System.out.println("Introduce el pin: ");
+                                pin=teclado.nextInt();
+                                MetodosVehiculos.retirarVehiculoAbonado(dni, matricula, pin);
                                 break;
 
                             default:
@@ -146,16 +173,60 @@ public class ParkingCliente {
                         Eleccion2=teclado.nextInt();
                         switch (Eleccion2) {
                             case 1:
-
+                                Admin.estadoPlazas();
                                 break;
+                                
                             case 2:
+                                do{
+                                    System.out.println("Elige un método de facturación: ");
+                                    System.out.println("1. Entre fechas");
+                                    System.out.println("2. Abonados");
+                                    Eleccion3=teclado.nextInt();
+                                    switch (Eleccion3) {
+                                            case 1:
+                                                Admin.PrecioEntreDosFechas(LocalDate.of(2015, Month.DECEMBER, 5), LocalDate.of(2019, Month.MARCH, 8), LocalTime.of(00, 0), LocalTime.of(23, 59));
+                                                break;
 
+                                            case 2:
+                                                Admin.abonosAnual();
+                                                break;
+                                            default:
+                                                Eleccion3=0;
+                                    }
+                                }while(Eleccion3==0);
                                 break;
+                                
                             case 3:
+                                do{
+                                    System.out.println("Elige una opción");
+                                    System.out.println("1. Dar de alta un abonado");
+                                    System.out.println("2. Modificar un abonado");
+                                    System.out.println("3. Eliminar un abonado");
+                                    System.out.println("4.Caducidad de abonos");
+                                    Eleccion4=teclado.nextInt();
 
+                                    switch (Eleccion4) {
+                                        case 1:
+                                            System.out.println("Introduce un número de mes: ");
+                                            mes=teclado.nextInt();
+                                            ArrayList<AbonadosVO> listaMes=Admin.mesQueCaduca(mes);
+                                            System.out.println("Abonados que caducan este mes: ");
+                                            listaMes.forEach(System.out::println);
+                                            break;
+
+                                        case 2:
+                                            ArrayList<AbonadosVO> lista10Dias=Admin.caduca10Dias();
+                                            System.out.println("Abonados que caducan en 10 días: ");
+                                            lista10Dias.forEach(System.out::println);
+                                            break;
+
+                                        default:
+                                            Eleccion4=0;
+                                    }
+                                }while(Eleccion4==0);
                                 break;
                             case 4:
-
+                                
                                 break;
                             default:
                                 System.out.println("Introduce una opción válida");
